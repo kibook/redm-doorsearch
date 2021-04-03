96ca123693faf5f545f6c53c4176400c3084d5f8
kibukj <kibuzhj@gmail.com>
2021-04-03 01:54:11 -0400

    Initial commit

---
+++ client.lua
@@ -0,0 +1,37 @@
+function AddDoorToSystemNew(doorHash, p1, p2, p3, threadId, p5, p6)
+	return Citizen.InvokeNative(0xD99229FE93B46286, doorHash, p1, p2, p3, threadId, p5, p6)
+end
+
+RegisterCommand('door', function(source, args, raw)
+	local door = tonumber(args[1])
+	
+	if not IsDoorRegisteredWithSystem(door) then
+		AddDoorToSystemNew(door, 1, 1, 0, 0, 0, 0)
+	end
+
+	DoorSystemSetDoorState(door, tonumber(args[2]))
+end)
+
+RegisterCommand('doorsearch', function(source, args, raw)
+	CreateThread(function()
+		local coords = GetEntityCoords(PlayerPedId())
+
+		local i = 0
+
+		print(string.format('Starting door search at %.2f, %.2f, %.2f...', coords.x, coords.y, coords.z))
+
+		for door, info in pairs(doorhashes) do
+			if #(coords - vector3(info[4], info[5], info[6])) <= 2.0 then
+				print(door)
+			end
+
+			if i % 100 == 0 then
+				Wait(0)
+			end
+
+			i = i + 1
+		end
+
+		print('Done')
+	end)
+end)
