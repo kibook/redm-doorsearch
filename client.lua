7d3e2c4d63c6a21f4f4f74fd1181258422cbc086
kibukj <kibuzhj@gmail.com>
2021-09-19 21:28:16 -0400

    Clean up code

--- client.lua
+++ client.lua
@@ -1,8 +1,8 @@
-function AddDoorToSystemNew(doorHash, p1, p2, p3, threadId, p5, p6)
-	return Citizen.InvokeNative(0xD99229FE93B46286, doorHash, p1, p2, p3, threadId, p5, p6)
+local function AddDoorToSystemNew(doorHash, p1, p2, p3, threadId, p5, p6)
+	return N_0xd99229fe93b46286(doorHash, p1, p2, p3, threadId, p5, p6)
 end
 
-RegisterNetEvent('doorsearch:setDoorState', function(door, state)
+RegisterNetEvent("doorsearch:setDoorState", function(door, state)
 	if not IsDoorRegisteredWithSystem(door) then
 		AddDoorToSystemNew(door, 1, 1, 0, 0, 0, 0)
 	end
@@ -10,13 +10,13 @@ RegisterNetEvent('doorsearch:setDoorState', function(door, state)
 	DoorSystemSetDoorState(door, state)
 end)
 
-RegisterNetEvent('doorsearch:search', function()
+RegisterNetEvent("doorsearch:search", function()
 	CreateThread(function()
 		local coords = GetEntityCoords(PlayerPedId())
 
 		local i = 0
 
-		print(string.format('Starting door search at %.2f, %.2f, %.2f...', coords.x, coords.y, coords.z))
+		print(string.format("Starting door search at %.2f, %.2f, %.2f...", coords.x, coords.y, coords.z))
 
 		for door, info in pairs(doorhashes) do
 			if #(coords - vector3(info[4], info[5], info[6])) <= 2.0 then
@@ -30,6 +30,6 @@ RegisterNetEvent('doorsearch:search', function()
 			i = i + 1
 		end
 
-		print('Done')
+		print("Done")
 	end)
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
-fx_version 'adamant'
-game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+fx_version "cerulean"
+game "rdr3"
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
-server_script 'server.lua'
+server_script "server.lua"
 
 client_scripts {
-	'doors.lua',
-	'client.lua'
+	"doors.lua",
+	"client.lua"
 }
